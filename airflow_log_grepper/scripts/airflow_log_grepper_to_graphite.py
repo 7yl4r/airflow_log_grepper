2cc88a0660b0126491c3813767a379edfc336c2b
Tylar Murray <murray.tylar@gmail.com>
2019-08-23 19:01:19 +0000

    mv glob sanitizer to module

--- airflow_log_grepper/scripts/airflow_log_grepper_to_graphite.py
+++ airflow_log_grepper/scripts/airflow_log_grepper_to_graphite.py
@@ -1,34 +1,9 @@
 import os
 import sys
 import subprocess
+
 from airflow_log_grepper.log_grepper import get_grepped_log_counts
-
-
-def sanitize_glob_string(glob_str):
-    """
-    Replaces characters in a glob string which might offend airflow's DAG and
-    task naming conventions or graphite's metric namespacing.
-
-    I.e. : convert glob strings to underscores, letters, numbers while trying
-           to preserve the meaning of the glob.
-
-   This works best if the glob_str uses only lowercase letters, because the
-   replacements use uppercase letters.
-    """
-    for offensive_char, replacement in [
-        ['*', 'X'],
-        ['[', 'I'],
-        [']', 'I'],
-        ['{', 'I'],
-        ['}', 'I'],
-        [',', 'I'],
-        ['.', 'O'],
-        ['-', 'T'],
-        ['?', 'Q'],
-        ['/', 'V'],
-    ]:
-        glob_str = glob_str.replace(offensive_char, replacement)
-    return glob_str
+from airflow_log_grepper.sanitize_glob_string import sanitize_glob_string
 
 
 def main():
